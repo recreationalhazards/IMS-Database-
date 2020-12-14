ac34c4fa1bf7086e408e1dfdf6a351a7c996effa
njorogeleah4@gmail.com <njorogeleah4@gmail.com>
2020-12-13 19:50:07 -0600

    adding jenkinsfile

--- src/main/java/com/cc/database/backend/phone/PhoneNumber.java
+++ src/main/java/com/cc/database/backend/phone/PhoneNumber.java
@@ -1,4 +1,23 @@
 package com.cc.database.backend.phone;
 
-public class PhoneNumber {
+import com.cc.database.backend.membership.model.Context;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+public class PhoneNumber implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String number;
+    private String countryCallingCode;
+    private String extension;
+    private String lineOfBusiness;
+
+
+
+
 }
